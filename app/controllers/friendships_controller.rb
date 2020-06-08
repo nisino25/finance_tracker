213ef156cfa769a320db499671a31edbe69e137d
nisino25 <nisino25@gmail.com>
2020-06-08 16:05:15 -0700

    finished

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,15 @@
 class FriendshipsController < ApplicationController
 
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Following friend"
+    else
+      flash[:alert] = "There was some errors"
+    end
+    redirect_to my_friends_path
+
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do 
-  
+
   resources :user_stocks, only:[:create, :destroy]
   devise_for :users
   root 'home#welcome'
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get 'my_friends', to: 'users#my_friends'
   get 'search_friend', to: 'users#search'
   resources :friendships, only:[:create, :destroy]
+  resources :users, only: [:show]
 end
